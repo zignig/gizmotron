56f10ddf614b236c2eae48b40f193196d15386f4
Simon Kirkby <obeygiantrobot@gmail.com>
2019-06-24 12:59:07 +0930

    add enable signal to counter

--- cores/counter.py
+++ cores/counter.py
@@ -1,14 +1,16 @@
-from .gizmo import Gizmo, IO
+from .gizmo import Gizmo, IO, BIT
 from nmigen import *
 
 
 class counter(Elaboratable):
     def __init__(self):
         self.counter = Signal(16)
+        self.en = Signal()
 
     def elaborate(self, platform):
         m = Module()
-        m.d.sync += self.counter.eq(self.counter + 1)
+        with m.If(self.en):
+            m.d.sync += self.counter.eq(self.counter + 1)
         return m
 
 
@@ -17,4 +19,7 @@ class Counter(Gizmo):
         c = counter()
         self.add_device(c)
         s = IO(sig_in=c.counter, name="counter")
+        e = IO(sig_in=c.en, name="enable")
+        e.add_bit(BIT("enable",0))
+        self.add_reg(e)
         self.add_reg(s)
