197e932f262d0db168b89499f96dca195044d0bb
Simon Kirkby <obeygiantrobot@gmail.com>
2019-06-25 15:41:28 +0930

    gtkwave sim shows working , need to test

--- asm/tx.asm
+++ asm/tx.asm
@@ -1,32 +1,57 @@
 J init
 ; Some delay macros 
 
+
 .equ tx_status,1
 .equ tx_data,2
 
 .def data , R0
 .def addr , R1
+.def d2 , R4
+.def zero , R6
+.def max,R3
+
+.string test,"this is a longer string to see if this works"
 
 .macro get_tx_status
 	MOVI addr,tx_status
 	LDX data,addr,0
 .endm
 
-.macro put_tx_data
+.macro ack_tx_status, value
+	MOVI addr,tx_status
+	MOVI data,$value
+	STX data,addr,0	
+.endm
+
+.macro set_ack
+	ack_tx_status 1
+.endm
+
+.macro clear_ack
+	ack_tx_status 0
+.endm
+
+.macro put_tx_data, data
 	MOVI addr,tx_data
+	MOV data,$data
 	STX data,addr,0
 .endm
 
 init:
-    MOVI R4,49
-    MOVI R5,60
+    MOVI d2,49
+    MOVI max,55
 loop:
     NOP
+    put_tx_data d2
+    set_ack
+    clear_ack 
 wait:
-    LDX R6, R1,0
-    CMP R3,R6
+    get_tx_status
+    CMP data,zero
     JE wait
-    MOV R4,R1
-    put_tx_data
+    ADDI d2,1
+    CMP d2,max
+    JE init
     J loop
 
