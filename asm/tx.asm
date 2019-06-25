1daa752f2e549c9fcc427969a36a87b81eba15fc
Simon Kirkby <obeygiantrobot@gmail.com>
2019-06-25 13:03:45 +0930

    testing simulation

--- asm/tx.asm
+++ asm/tx.asm
@@ -1,42 +1,14 @@
 J init
 ; Some delay macros 
 
-wait:
-    SUBI R5, 1
-    CMP R5,R6
-    JNZ wait
-    JR R7,0
-
-.macro delay 
-    MOVL R5,255
-    MOVH R5,255
-    JAL R7,wait
-.endm
-
-.macro multi
-    delay
-    delay
-    delay
-    delay
-.endm 
-.string bob, "this is test"
-
-; 
 init:
-    MOVL R4,0 
-    MOVH R4,48
-    MOVL R1,90
-    MOVH R1,0
+    MOVI R0,2
+    MOVI R4,49
+    MOVI R5,60
 loop:
-    MOVL R0,1
     STX R4, R0, 0 
-    MOVH R0,1
-    STX R3, R0, 0 
-    MOVL R3,0     
-    STX R3, R0, 0 
-    CMP R1,R4
+    CMP R4,R5
     JE init
     ADDI R4,1
-    delay
     J loop
 
---
+++ sim.py
@@ -0,0 +1,64 @@
+import itertools
+from nmigen import *
+
+from processor import Boneless
+
+# Working gizmos
+from cores.gizmo import TestGizmo
+from cores.user_leds import UserLeds
+from cores.serial import Serial
+from cores.counter import Counter
+from cores.pwm import Pwm
+
+
+class CPU(Elaboratable):
+    def __init__(self, platform, asm_file="asm/tx.asm"):
+        b = Boneless(asm_file=asm_file)
+        self.b = b
+        self.platform = platform
+
+        # TODO gizmo needs **Kwargs , to add extra variables to gizmos
+
+        l = UserLeds("leds", platform=platform)
+        b.add_gizmo(l)
+
+        s = Serial(
+            "serial_port", platform=platform, number=0, baud=9600
+        )  # should pass baud
+        b.add_gizmo(s)
+
+        c = Counter("counter1", platform=platform)
+        b.add_gizmo(c)
+
+        c2 = Counter("counter2", platform=platform)
+        b.add_gizmo(c2)
+
+        # p = Pwm("pwm",platform=platform,pin=12)
+        # b.add_gizmo(p)
+
+        # Assign addresses , get code etch
+        # TODO test and fix
+        # TODO integrate assembler
+        self.b.prepare()
+
+    def elaborate(self, platform):
+        clk16 = platform.request("clk16", 0)
+
+        m = Module()
+        m.submodules.boneless = self.b
+        return m
+
+
+if __name__ == "__main__":
+    from plat import BB
+
+    import argparse
+    from nmigen import cli
+
+    parser = argparse.ArgumentParser()
+    cli.main_parser(parser)
+    args = parser.parse_args()
+    platform =BB()
+    tb = CPU(platform)
+    ios = ()
+    cli.main_runner(parser, args, tb, platform=platform,name="boneless_core", ports=ios)
