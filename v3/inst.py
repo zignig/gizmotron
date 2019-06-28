b159cde1cd483e6a49fcf21bccc3cb77e74544be
Simon Kirkby <obeygiantrobot@gmail.com>
2019-06-28 22:03:28 +0800

    nearly working

--- v3/inst.py
+++ v3/inst.py
@@ -3,32 +3,34 @@ import opcode_v3  as op
 OPCODE_BITS = 5
 INSTRUCTION_SIZE = 16
 REGISTERS = 8
+debug = False
 
+# symbol resolving function
+def res(name):
+    print("NO RESOLVER FOR ",name)
+
+resolver = res
 
 class grain:
     bits = 1 
+    _resolve = False
     def __init__(self,val):
-        self.val = val
         if isinstance(val,str):
-            print('string')
+            # symbol resolution
+            self.val = val
+            self._resolve = True
         if isinstance(val,int):
             assert val < 2**self.bits
-        print(type(val),self.val)
+            self.val = val
 
 class opcode(grain):
     bits = OPCODE_BITS 
 
-class op4(opcode):
+class op4(grain):
     bits = 4 
 
-
-class op3(opcode):
+class op3(grain):
     bits = 3
-
-
-class instruction:
-    bits = INSTRUCTION_SIZE
-    opcodes = []
 
         
 class register(grain):
@@ -66,6 +68,35 @@ class imm13(grain):
     bits = 13
 
 
+class instruction:
+    bits = INSTRUCTION_SIZE
+    opcodes = []
+    @property
+    def val(self):
+        r = self.bits 
+        val = 0 
+        #print("--- decompose ---")
+        for i in self.build[:-1]:
+            #print(i,r,i.bits,i.val,"|",bin(i.val),"|")
+            if i._resolve:
+                i.val = resolver(i.val)
+                if i.val is None:
+                    raise
+            r = r - i.bits
+            val += i.val << r
+            #print('{:016b}'.format(val))
+        i = self.build[-1:][0]
+        if i._resolve:
+            last = resolver(i.val)
+            if last is None:
+                raise 
+        else:
+            last = i.val
+        val += last
+        #print('final')
+        print('{:016b}'.format(val))
+        return val
+
 class RRR(instruction):
     def __init__(self, opc,m,rsd,ra,t,rb):
         self.opcode = op4(opc)
@@ -74,17 +105,18 @@ class RRR(instruction):
         self.Ra = register(ra)
         self.t = _type(t)
         self.Rb = register(rb)
-        self.build = [self.opcode, self.Rsd, self.Ra, self.t, self.Rb]
+        self.build = [self.opcode,self.mode, self.Rsd, self.Ra, self.t, self.Rb]
 
 
 class RR3(instruction):
-    def __init__(self, opc, rsd, ra, t, im):
-        self.opcode = opcode(opcode)
+    def __init__(self, opc,m, rsd, ra, t, im):
+        self.opcode = op4(opc)
+        self.mode = mode(m)
         self.Rsd = register(rsd)
         self.Ra = register(ra)
         self.t = _type(t)
         self.i3 = imm3(im)
-        self.build = [self.opcode, self.Rsd, self.Ra, self.t, self.i3]
+        self.build = [self.opcode, self.mode,self.Rsd, self.Ra, self.t, self.i3]
 
 
 class RR5(instruction):
@@ -129,6 +161,53 @@ class X(instruction):
 
 
 
-
+# logic
 def AND(ra, rb, rd): return RRR(op.OPCODE4_LOGIC,0, ra, rb, 0b00, rd)
-def ANDI(ra, rb, rd): return RRR(op.OPCODE4_LOGIC,1, ra, rb, 0b00, rd)
+def ANDI(ra, rb, im): return RR3(op.OPCODE4_LOGIC,1, ra, rb, 0b00, im)
+def OR(ra, rb, rd): return RRR(op.OPCODE4_LOGIC,0, ra, rb, 0b01, rd)
+def ORI(ra, rb, im): return RR3(op.OPCODE4_LOGIC,1, ra, rb, 0b01, im)
+def XOR(ra, rb, rd): return RRR(op.OPCODE4_LOGIC,0, ra, rb, 0b10, rd)
+def XORI(ra, rb, im): return RR3(op.OPCODE4_LOGIC,1, ra, rb, 0b10, im)
+def CMP(ra, rb, rd): return RRR(op.OPCODE4_LOGIC,0, ra, rb, 0b11, rd)
+def CMPI(ra, rb, im): return RR3(op.OPCODE4_LOGIC,1, ra, rb, 0b11, im)
+
+# arith
+def ADD(ra, rb, rd): return RRR(op.OPCODE4_ARITH,0, ra, rb, 0b00, rd)
+def ADDI(ra, rb, im): return RR3(op.OPCODE4_ARITH,1, ra, rb, 0b00, im)
+def ADC(ra, rb, rd): return RRR(op.OPCODE4_ARITH,0, ra, rb, 0b01, rd)
+def ADCI(ra, rb, im): return RR3(op.OPCODE4_ARITH,1, ra, rb, 0b01, im)
+def SUB(ra, rb, rd): return RRR(op.OPCODE4_ARITH,0, ra, rb, 0b10, rd)
+def SUBI(ra, rb, im): return RR3(op.OPCODE4_ARITH,1, ra, rb, 0b10, im)
+def SBB(ra, rb, rd): return RRR(op.OPCODE4_ARITH,0, ra, rb, 0b11, rd)
+def SBBI(ra, rb, im): return RR3(op.OPCODE4_ARITH,1, ra, rb, 0b11, im)
+
+# shift
+def SLL(ra, rb, rd): return RRR(op.OPCODE4_SHIFT,0, ra, rb, 0b00, rd)
+def SLLI(ra, rb, im): return RR3(op.OPCODE4_SHIFT,1, ra, rb, 0b00, im)
+def ROT(ra, rb, rd): return RRR(op.OPCODE4_SHIFT,0, ra, rb, 0b01, rd)
+def ROTI(ra, rb, im): return RR3(op.OPCODE4_SHIFT,1, ra, rb, 0b01, im)
+def SRL(ra, rb, rd): return RRR(op.OPCODE4_SHIFT,0, ra, rb, 0b10, rd)
+def SRLI(ra, rb, im): return RR3(op.OPCODE4_SHIFT,1, ra, rb, 0b10, im)
+def SRA(ra, rb, rd): return RRR(op.OPCODE4_SHIFT,0, ra, rb, 0b11, rd)
+def SRAI(ra, rb, im): return RR3(op.OPCODE4_SHIFT,1, ra, rb, 0b11, im)
+
+# mem
+
+# jumps
+
+# extended
+def EXTI(imm): return E(op.OPCODE3_EXTI,imm)
+# new commands
+
+# testing 
+ADD(0,0,0).val
+ADD(7,7,7).val
+
+
+
+
+
+
+
+
+
