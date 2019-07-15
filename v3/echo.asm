1d8a4c0e47e8e2261eeea37a3d0050325352c619
Simon Kirkby <obeygiantrobot@gmail.com>
2019-07-15 17:20:31 +0930

    more echo

---
+++ v3/echo.asm
@@ -0,0 +1,26 @@
+; basic echo 
+.window 
+J init
+; R0 = data
+; R1 = char
+; R7 = return address
+
+waitrxchar: 		; wait for a char on serial 
+	LDXA R0,1	; load the rx status
+	CMPI R0,1	; is it 1
+	JZ waitrxchar	; if 
+	LDXA R1,2	; load the data into chear 
+	JR R7,0		; return from the sub 
+
+txchar:			; transmit a char
+	STXA R1,4	; store the char in the output regiter 
+	MOVI R0,1	; 
+	STXA R0,3
+; Main Loop
+waittxack:
+	LDXA R2,3
+	CMPI R2,1
+	JE waittxack
+init:
+	JAL R7,waitrxchar
+J init
