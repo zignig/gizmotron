a6481dc48c068d59279e83e3208c88d682ce151c
Simon Kirkby <obeygiantrobot@gmail.com>
2019-07-28 16:30:59 +0800

    more noodling with arpeggio PEG parser

---
+++ compiler/calc.py
@@ -0,0 +1,110 @@
+#######################################################################
+# Name: calc.py
+# Purpose: Simple expression evaluator example
+# Author: Igor R. Dejanovic <igor DOT dejanovic AT gmail DOT com>
+# Copyright: (c) 2009-2015 Igor R. Dejanovic <igor DOT dejanovic AT gmail DOT com>
+# License: MIT License
+#
+# This example demonstrates grammar definition using python constructs as
+# well as using semantic actions to evaluate simple expression in infix
+# notation.
+#######################################################################
+
+from __future__ import unicode_literals, print_function
+try:
+    text=unicode
+except:
+    text=str
+
+from arpeggio import Optional, ZeroOrMore, OneOrMore, EOF, \
+    ParserPython, PTNodeVisitor, visit_parse_tree
+from arpeggio import RegExMatch as _
+
+def number():     return _(r'\d*\.\d*|\d+')
+def symbol():           return _(r"\w+")
+def factor():     return Optional(["+","-"]), [number,symbol,("(", expression, ")")]
+def term():       return factor, ZeroOrMore(["*","/"], factor)
+def expression(): return term, ZeroOrMore(["+", "-"], term)
+def calc():       return OneOrMore(expression), EOF
+
+
+class CalcVisitor(PTNodeVisitor):
+
+    def visit_number(self, node, children):
+        """
+        Converts node value to float.
+        """
+        if self.debug:
+            print("Converting {}.".format(node.value))
+        return float(node.value)
+
+    def visit_symbol(self,node,children):
+        return 
+
+    def visit_factor(self, node, children):
+        """
+        Applies a sign to the expression or number.
+        """
+        if self.debug:
+            print("Factor {}".format(children))
+        if len(children) == 1:
+            return children[0]
+        sign = -1 if children[0] == '-' else 1
+        return sign * children[-1]
+
+    def visit_term(self, node, children):
+        """
+        Divides or multiplies factors.
+        Factor nodes will be already evaluated.
+        """
+        if self.debug:
+            print("Term {}".format(children))
+        term = children[0]
+        for i in range(2, len(children), 2):
+            if children[i-1] == "*":
+                term *= children[i]
+            else:
+                term /= children[i]
+        if self.debug:
+            print("Term = {}".format(term))
+        return term
+
+    def visit_expression(self, node, children):
+        """
+        Adds or substracts terms.
+        Term nodes will be already evaluated.
+        """
+        if self.debug:
+            print("Expression {}".format(children))
+        expr = children[0]
+        for i in range(2, len(children), 2):
+            if i and children[i - 1] == "-":
+                expr -= children[i]
+            else:
+                expr += children[i]
+        if self.debug:
+            print("Expression = {}".format(expr))
+        return expr
+
+
+def main(debug=False):
+    # First we will make a parser - an instance of the calc parser model.
+    # Parser model is given in the form of python constructs therefore we
+    # are using ParserPython class.
+    parser = ParserPython(calc)#, debug=debug)
+
+    # An expression we want to evaluate
+    input_expr = "bob-(4-1)*5+(2+4.67)+5.89/(.2+7)"
+
+    # We create a parse tree out of textual input_expr
+    while(True):
+        t = input(">")
+        parse_tree = parser.parse(t)
+        result = visit_parse_tree(parse_tree, CalcVisitor(debug=debug))
+        print(result)
+
+if __name__ == "__main__":
+    # In debug mode dot (graphviz) files for parser model
+    # and parse tree will be created for visualization.
+    # Checkout current folder for .dot files.
+    main(debug=True)
---
+++ compiler/trie.py
@@ -0,0 +1,88 @@
+#######################################################################
+# Name: simple.py
+# Purpose: Simple language based on example from pyPEG
+# Author: Igor R. Dejanovic <igor DOT dejanovic AT gmail DOT com>
+# Copyright: (c) 2009-2015 Igor R. Dejanovic <igor DOT dejanovic AT gmail DOT com>
+# License: MIT License
+#
+# This example demonstrates grammar definition using python constructs.
+# It is taken and adapted from pyPEG project (see http://www.fdik.org/pyPEG/).
+#######################################################################
+
+from __future__ import unicode_literals
+
+import os
+from arpeggio import *
+from arpeggio import RegExMatch as _
+
+# Grammar
+def nl():               return '\n'
+def symbol():           return _(r"\w+")
+def subsection():        return "-",symbol,nl
+def ref():              return '@',symbol,nl
+def section():          return ":",symbol,nl,OneOrMore([subsection,ref])
+def trie():   return OneOrMore(section),EOF
+
+def comment():          return [_("//.*"), _("/\*.*\*/")]
+
+class Entry:
+    def __init__(self,value,children=None):
+        self.value = value
+        self._more = False
+        if children is not None:
+            self._more = True
+        self.children = children
+
+    def eval(self):
+        print(self.value)
+        if self._more:
+            for i in self.children:
+                i.eval()
+
+class Symbol(Entry):
+    pass
+
+class Subsection(Entry):
+    pass
+
+class Ref(Entry):
+    pass
+
+class Section(Entry):
+    pass
+ 
+class Vis(PTNodeVisitor):
+
+    def visit_symbol(self,node,children):
+        #print('symbol->',node,children)
+        return Symbol(node) 
+ 
+    def visit_section(self,node,children):
+        print('section->',children)
+        return Section(node,children=children)
+
+    def visit_subsection(self,node,children):
+        print('subsection->',children)
+        return Subsection(node,children)
+
+    def visit_ref(self,node,children):
+        #print('ref->',node,children)
+        return Ref(node)
+
+    def visit_trie(self,node,children):
+        return Section(node,children=children)
+
+debug=False
+# Load test program from file
+current_dir = os.path.dirname(__file__)
+test_program = open(os.path.join(current_dir, 'menu.tri')).read()
+
+# Parser instantiation. simpleLanguage is the definition of the root rule
+# and comment is a grammar rule for comments.
+parser = ParserPython(trie, comment, debug=debug,ws="\t\r ")
+
+parse_tree = parser.parse(test_program)
+print(parse_tree)
+result = visit_parse_tree(parse_tree,Vis(debug=debug))
+print(result)
+result.eval()
