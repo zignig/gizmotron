001aa991a90e621e4ad488000331679554434811
Simon Kirkby <obeygiantrobot@gmail.com>
2019-07-29 09:56:59 +0930

    compiler stuff

--- compiler/simple.py
+++ compiler/simple.py
@@ -28,13 +28,24 @@ def expressionlist():   return expression, ZeroOrMore(",", expression)
 def returnstatement():  return Kwd("return"), expression
 def ifstatement():      return Kwd("if"), "(", expression, ")", block
 def ifelsestatement():  return Kwd("if"), "(", expression, ")", block, Kwd("else"), block
-def statement():        return [assign,ifelsestatement,ifstatement,returnstatement], ";"
+def functioncall():     return symbol,parameterlist
+def statement():        return [assign,ifelsestatement,ifstatement,functioncall,returnstatement], ";"
 def block():            return "{", OneOrMore(statement), "}"
 def parameterlist():    return "(", symbol, ZeroOrMore(",", symbol), ")"
-def functioncall():     return symbol, "(", expressionlist, ")"
 def function():         return Kwd("function"), symbol, parameterlist, block
 def simpleLanguage():   return OneOrMore(function),EOF
 
+
+class iffer:
+    def __init__(self,condition,statement,elses=None):
+        self.condition = condition
+        self.statement = statement
+        self.elses = elses
+
+class fcall:
+    def __init__(self,name,param):
+        self.name = name
+        self.param = param
 
 class lit:
     def __init__(self,value):
@@ -45,8 +56,12 @@ class op:
         self.op = op
 
 class sym:
+    # all symbols are global
+    s = {}
     def __init__(self,symb):
         self.symbol = symb
+        if symb not in self.s:
+            self.s[symb] = self
 
 class param:
     def __init__(self,l):
@@ -58,33 +73,54 @@ class fun:
         self.params = params
         self.statements = statements
 
+    def eval(self):
+        for i in self.statements:
+            print(i)
+            i.eval()
+
+class assignc:
+    def __init__(self,left,right):
+        self.left = left
+        self.right = right
+
 class Vis(PTNodeVisitor):
 
+    def visit_ifstatement(self,node,children):
+        #print('if',node,children)
+        return iffer(children[0],children[1])
+
+    def visit_ifelsestatement(self,node,children):
+        #print('ifelse',node,children)
+        return iffer(children[0],children[1],elses=children[2])
+
     def visit_assign(self,node,children):
-        print('assignment',node,children)
+        #print('assignment',node,children)
+        return assignc(children[0],children[2])
 
     def visit_symbol(self,node,children):
-        return sym(node)
+        #print(node.value)
+        return sym(node.value)
 
     def visit_parameterlist(self,node,children):
-        print('param',children)
+        #print('param',children)
         return param(children)
 
     def visit_literal(self,node,children):
-        print('lit',node,children)
+        #print('lit',node,children)
         return lit(node)
+
+    def visit_statement(self,node,children):
+        #print('statement',node,children)
+        return children
 
     def visit_block(self,node,children):
         return children
-
-    def visit_statement(self,node,children):
-        return children
-
+    
     def visit_operator(self,node,children):
         return op(node)
 
     def visit_expression(self,node,children):
-        print('expr', node,children)
+        #print('expr', node,children)
         return children
 
     def visit_function(self,node,children):
@@ -92,7 +128,8 @@ class Vis(PTNodeVisitor):
         return fun(children[0],children[1],children[2])
 
     def visit_functioncall(self,node,children):
-        print(node,children)
+        #print("call",node,children)
+        return None
 
     def visit_simpleLanguage(self,node,children):
         return children
@@ -107,6 +144,8 @@ test_program = open(os.path.join(current_dir, 'program.simple')).read()
 parser = ParserPython(simpleLanguage, comment, debug=debug)
 
 parse_tree = parser.parse(test_program)
-print(parse_tree)
+#print(parse_tree)
 result = visit_parse_tree(parse_tree,Vis(debug=debug))
-print(result)
+for i in result:
+    print(i.eval())
+print(sym.s.keys())
