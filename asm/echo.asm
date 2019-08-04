fc1da08427a837898d99dbc68d289edcdd19040f
Simon Kirkby <obeygiantrobot@gmail.com>
2019-08-04 08:27:04 +0800

    some comments

--- asm/echo.asm
+++ asm/echo.asm
@@ -6,36 +6,36 @@ J init
 ; Basic echo construct
 ; Need macros and register renames
 
-checkrx:
-    LDXA R3,rx_status
-    CMPI R1,1
-    JE addtopad
-    JR R7,0  ; no change in status , jump back to main loop
+checkrx:                        ; get a char off the serial port 
+    LDXA R3,rx_status           ; load the RX status from the serial port
+    CMPI R3,1                   ; compare the register to 1
+    JE addtopad                 ; if it is equal to one ,  add it to the pad
+    JR R7,0                     ; no change in status , jump back to main loop
 addtopad:
-    LDXA R2,rx_data
-    MOVI R3,1
-    STXA R3,rx_status
-    STXA R2,blinky
+    LDXA R2,rx_data             ; load the RX data from the serial port
+    MOVI R3,1                   ; load 1 into to R3
+    STXA R3,rx_status           ; acknowledge the char in the serial port
+    STXA R2,blinky              ; write to blinky
 JR R7,0
 
-txchar:
-    STXA R2,tx_data
-    MOVI R3,1
-    STXA R3,tx_status
-    MOVI R3,0
-    STXA R3,tx_status
-waitup:
-    LDXA R3,tx_status
+txchar:                         ; put a char into the serial port 
+    STXA R2,tx_data             ; put the holding data into the serial port
+    MOVI R3,1                   ; set status to one
+    STXA R3,tx_status           ; write to tx status 
+    MOVI R3,0                   ; set the status to zerop
+    STXA R3,tx_status           ; acknowledge the wrtie 
+waitup:                         ; TODO , this should be a fifo in the serial port 
+    LDXA R3,tx_status           ; wait for tx status to go high
     CMPI R3,1
     JE waitup
 waitdown:
-    LDXA R3,tx_status
+    LDXA R3,tx_status           ; wait for the status to go low
     CMPI R3,0
     JE waitdown
 JR R7,0
 
-init:
-    MOVI R0,0 ; working register 
+init:                           ; initialize the program all the registers.
+    MOVI R0,0 ; working register        
     MOVI R1,0 ; working address 
     MOVI R2,32; holding data 
     MOVI R3,0 ; device status
@@ -44,11 +44,11 @@ init:
     MOVI R6,0 ; 
     MOVI R7,0 ; jump return address
 
-run:
-    JAL R7,txchar
-
-    CMPI R2,126 ; loop through readable chars
-    JE resetChar
+run:                                   ; main loop
+    JAL R7,txchar                      ; write r2 ( holding ) to the serial port 
+    JAL R7,checkrx                      ; get a char from the serial port
+    CMPI R2,126                         ; loop through readable chars
+    JE resetChar                        ; this is TX testing 
     ADDI R2,R2,1
     J run
 resetChar:
