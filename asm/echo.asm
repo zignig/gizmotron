ff9bdfb4b1af8535343c3b0df2426e5de13339a2
Simon Kirkby <obeygiantrobot@gmail.com>
2019-08-04 11:50:37 +0800

    start of pad writer

--- asm/echo.asm
+++ asm/echo.asm
@@ -1,9 +1,11 @@
 .window
 J init
-.alloc leds,1
-.alloc pad_count,1
-.alloc pad,32
-.equ delay,1
+.alloc padStatus,1      ; is the pad ready to go ?
+.alloc padCount,1       ; cursor for the pad
+.alloc pad,32           ; the pad itself 
+
+.equ delay,65000        ; constant for delay
+
 ; Basic echo construct
 ; Need macros and register renames
 
@@ -13,20 +15,29 @@ checkrx:                        ; get a char off the serial port
     JE addtopad                 ; if it is equal to one ,  add it to the pad
     JR R7,0                     ; no change in status , jump back to main loop
 addtopad:
+    ; get the data and acknowledge
     LDXA R2,rx_data             ; load the RX data from the serial port
     MOVI R3,1                   ; load 1 into to R3
     STXA R3,rx_status           ; acknowledge the char in the serial port
     MOVI R3,0                   ; load 0 into to R3
     STXA R3,rx_status           ; acknowledge the char in the serial port
-    STXA R2,blinky              ; write to blinky
+    ; put the incoming char into the pad
+    MOVR R1,pad                 ; load the pad address into the working address
+    MOVR R0,padCount            ; load the padcount address
+    LD R5,R1,0                  ; load the padcount value into the pad pointer 
+    ADD R5,R5,R1                ; add the padcount and pad address to get the current pad address 
+    ST R2,R5,0                  ; store the value into the pad
+    ADDI R0,R0,1                ; increment the pad value
+    ST R0,R1,0                  ; put the value back into the pad varliable
+    STXA R0,blinky              ; write to blinky
 JR R7,0
 
 txchar:                         ; put a char into the serial port 
     STXA R2,tx_data             ; put the holding data into the serial port
     MOVI R3,1                   ; set status to one
     STXA R3,tx_status           ; write to tx status 
-    MOVI R3,0                   ; set the status to zerop
-    STXA R3,tx_status           ; acknowledge the wrtie 
+    MOVI R3,0                   ; set the status to zero
+    STXA R3,tx_status           ; acknowledge the write 
 waitup:                         ; TODO , this should be a fifo in the serial port 
     LDXA R3,tx_status           ; wait for tx status to go high
     CMPI R3,1
@@ -43,7 +54,7 @@ init:                           ; initialize the program all the registers.
     MOVI R2,32; holding data 
     MOVI R3,0 ; device status
     MOVI R4,0 ; delayer
-    MOVI R5,0 ; 
+    MOVI R5,0 ; pad pointer
     MOVI R6,0 ; 
     MOVI R7,0 ; jump return address
 
