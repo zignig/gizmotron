7a522db3da3b4d522608b7ba307e5be638a69b88
Simon Kirkby <obeygiantrobot@gmail.com>
2019-08-06 20:41:25 +0800

    working greeter and echo into pad wq

--- asm/echo.asm
+++ asm/echo.asm
@@ -1,3 +1,13 @@
+; automatic gizmo headers
+;.equ blinky,0
+;.equ user_led,1
+;.equ tx_status,2
+;.equ tx_data,3
+;.equ rx_status,4
+;.equ rx_data,5
+;.equ image,6
+;.equ boot,7
+
 .window
 J init
 .alloc leds,1
@@ -35,9 +45,10 @@ padContinue:
     ; put the incoming char into the pad
     MOVR R1,pad		 ; move pad into the working address
     LD R0,R1,0		 ; load the length into the working register
-    AND R2,R1,R1	 ; copy the padd address into holding
-    ADD R2,R2,R0	 ; add the current pad length to holding
-    ST R3,R2,0		 ; store the word into to address in holding
+    AND R5,R1,R1	 ; copy the padd address into holding
+    ADD R5,R5,R0	 ; add the current pad length to holding
+    ST R2,R5,0		 ; store the word into to address in holding
+    STXA R5,blinky
     ADDI R0,R0,1	 ; increment to pad count
     ST R0,R1,0		 ; store the value back into the start of the pad
 JR R7,0
@@ -59,6 +70,15 @@ waitdown:
 JR R7,0
 
 
+nextchar:
+    LD   R2,R1,0        ; load the data at working address into holding
+    STXA R1,blinky
+    JAL   R7,txchar      ; write the char
+    ADDI R1,R1,1        ; increment the pointer
+    CMPI R2,0           ; look for a null (0) 
+    JNE  nextchar     ; not there yet, get next char
+JR R6,0 ; return with jump2
+
 init:                           ; initialize the program all the registers.
     MOVI R0,0 ; working register        
     MOVI R1,0 ; working address 
@@ -71,23 +91,12 @@ init:                           ; initialize the program all the registers.
 
     ; write the greet string
     MOVR R1,greet
-    JAL R6,writestring 
+    JAL R6,nextchar
 run:                                   ; main loop
     JAL R7,checkrx                     ; get a char from the serial port
     JAL R7,txchar                      ; write R2 ( holding ) to the serial port 
 J run
-
-writestring:
-    LD   R0,R1,0   ; move the length of the string into the working register
-    AND  R5,R1,R1  ; copy the address into a temp register
-    ADD  R5,R5,R0  ; calculate the end of the string
-nextchar:
-    ADDI R1,R1,1 ; increment the pointer
-    LD   R2,R1,0   ; load the data at working address into holding
-    JR   R7,txchar ; write the char
-    CMP  R1,R5    ; compare the current address to the end of the string
-    JNE  nextchar ; not there yet, get next char
-JR R6,0 ; return with jump2
+    
     
 .alloc pad,32 ; the pad itself 
-.string greet,Boneless-v3-zignig-bootloader
+.string greet,"Boneless-v3-zignig-bootloader\r\n\r\n\r\n>>"
