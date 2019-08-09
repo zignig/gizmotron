56618498bea035c37a9d476efd7a048bb33020ea
Simon Kirkby <obeygiantrobot@gmail.com>
2019-08-09 11:14:29 +0930

    auto build and breakup of simulator

---
+++ utils/gen_opcodes.py
@@ -0,0 +1,23 @@
+from boneless.arch.opcode import Instr
+
+names = Instr.mnemonics.keys()
+
+text = """
+from simi import SimInstr
+
+class Missing(BaseException):
+    pass
+
+
+"""
+sim_dict = "sim_dict = {\n" 
+for i in names:
+    head = "class s_"+i+"(SimInstr):\n"
+    head += "   def run(self):\n"
+    head += "       raise Missing\n\n"
+    text += head
+    sim_dict += '\t"'+i+'": s_'+i+',\n'
+sim_dict += '}\n'
+
+print(text)
+print(sim_dict)
---
+++ utils/simi.py
@@ -0,0 +1,41 @@
+
+class SimInstr(object):
+    def __init__(self,instr,sim):
+        self.instr = instr
+        self.sim = sim
+
+    def call(self):
+        if self.sim.has_exti:
+            print("Exetended instruction")
+            self.sim.has_exti = False
+            #TODO fix this
+        else:
+            self.run()
+
+    def __repr__(self):
+        return self.instr.__repr__()
+    
+    # short name access
+    @property
+    def rsd(self):
+        return self.instr.rsd.value
+
+    @property
+    def imm(self):
+        return self.instr.imm.value
+
+    @property
+    def ra(self):
+        return self.instr.ra.value 
+
+    def set_reg(self,reg,val):
+        self.sim.set_reg(reg,val)
+
+    def reg(self,reg):
+        return self.sim.get_reg(reg)
+
+    # simulator function
+    @property
+    def pc(self):
+        return self.sim.pc
+
