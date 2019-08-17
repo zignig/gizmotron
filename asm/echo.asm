c5cc6c838ae264e59cc048a3d47fd9b87f155694
Simon Kirkby <obeygiantrobot@gmail.com>
2019-08-17 19:11:42 +0800

    coverted back to length prefixed strings , working

--- asm/echo.asm
+++ asm/echo.asm
@@ -8,12 +8,10 @@
 ;.equ image,6
 ;.equ boot,7
 
-.window                 ; todo this macro needs to align to 8 word boundary
-J init
-spacer: .alloc 500
-leds: .alloc 1
+.window                 ; todo this macro needs to align to 8 word boundary, need to move window for bootloader
+J init                  ; jump to init
+spacer: .alloc 500      ; spacer for the new program , asm needs to be able to link to the bottom
 padStatus: .alloc 1      ; is the pad ready to go ?
-padCount: .alloc 1       ; cursor for the pad
 padCursor: .alloc 1       ; current position in the pad 
 ; pad itself is declared at the bottom so it does not overwrite code 
 .equ delay,65000        ; constant for delay
@@ -32,11 +30,12 @@ init:                           ; initialize the program all the registers.
 
     ; write the greet string
     MOVR R1,nl                  ; newline
-    JAL R6,nextchar
+    JAL R6,dumpstring
     MOVR R1,greet               ; greetings string
-    JAL R6,nextchar
+    JAL R6,dumpstring
     MOVR R1,pwd                 ; prompt
-    JAL R6,nextchar
+    JAL R6,dumpstring
+
 run:                            ; main loop
     JAL R7,checkrx              ; get a char from the serial port
     JAL R7,txchar               ; write R2 ( holding ) to the serial port 
@@ -45,7 +44,7 @@ J run
 
 warmboot:
     MOVR R1,wb                  ; write the warmboot string to the console
-    JAL R6,nextchar
+    JAL R6,dumpstring
     MOVI R0,0                   ; set boot image to 0
     STXA R0,image               ; write to the image external register
     MOVI R0,1                   ; write one into regiters
@@ -80,10 +79,10 @@ padContinue:
     ; put the incoming char into the pad
     MOVR R1,pad		        ; move pad into the working address
     LD R0,R1,0		        ; load the length into the working register
+    ADDI R0,R0,1	        ; increment to pad count
     AND R5,R1,R1	        ; copy the padd address into holding
     ADD R5,R5,R0	        ; add the current pad length to holding
     ST R2,R5,0		        ; store the word into to address in holding
-    ADDI R0,R0,1	        ; increment to pad count
     ST R0,R1,0		        ; store the value back into the pad counter 
 JR R7,0
 
@@ -105,47 +104,55 @@ waitdown:
     JE waitdown
 JR R7,0
 
-; strings are null terminated
+; strings are pascal style string first word is the length of the string. 
+dumpstring:
+    LD R0,R1,0                  ; load the length of the string into R0 
+    CMPI R0,0
+    JE exitdump
+    AND R5,R1,R1                ; copy address into temp
+    ADD R5,R5,R0                ; add the length to the address
+    ; R1 is the current address
+    ; R5 is the end of the string
 nextchar:
-    LD   R2,R1,0                ; load the data at working address into holding
-    JAL   R7,txchar             ; write the char
     ADDI R1,R1,1                ; increment the pointer
-    CMPI R2,0                   ; look for a null (0) 
-    JNE  nextchar               ; not there yet, get next char
+    LD   R2,R1,0                ; load the data at working address into holding
+    JAL  R7,txchar              ; write the char
+    CMP  R1,R5                  ; compare current with end of string
+    JULT nextchar               ; not there yet, get next char
+exitdump:
 JR R6,0                         ; return with jump2
-
-;dumppad: ; just dump the pad to console
-    ; TODO
-    ;MOVR R1,pad		 ; move pad into the working address
-    ;LD R0,R1,0		 ; load the length into the working register
-    ;AND R5,R1,R1	 ; copy the padd address into holding
-    ;ADD R5,R5,R0	 ; add the current pad length to holding
-;JR R6,0
     
+; Process the pad
 procpad:
     MOVR R1,padStatus	; load the pad status address into R1
     LD R0,R1,0		; load the pad status into R0
     CMPI R0,1		; is the pad active
     JE procPadContinue  ; continue
-    J run
+    J run               ; return to run
 procPadContinue:
     ; process the pad here 
-     
-    ; TODO write pad
-    ; JAL R6,dumppad
-
+    ; just dump to console for now 
     MOVR R1,nl          ; write a newline
-    JAL R6,nextchar     
+    JAL R6,dumpstring
+    MOVR R1,pad
+    JAL R6,dumpstring 
+    MOVR R1,nl          ; write a newline
+    JAL R6,dumpstring
+
     MOVR R1,pwd         ; write the console prompt
-    JAL R6,nextchar
+    JAL R6,dumpstring
     MOVR R1,padStatus   ; reset pad status 
     MOVI R0,0
     ST R0,R1,0
-    MOVR R1,padCount         ; reset pad counter 
+    MOVR R1,pad ; reset pad counter 
     MOVI R0,0
     ST R0,R1,0
 J run
-    
+
+; TODO what to do with the pad ? 
+;
+;
+
 greet: .string "Boneless-v3-zignig-bootloader\r\n"
 nl: .string "\r\n"
 pwd: .string "$ "
