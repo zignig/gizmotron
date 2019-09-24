67694c568e62909eb5d2495501dfdbc7d1b14e0e
Simon Kirkby <obeygiantrobot@gmail.com>
2019-09-24 18:21:47 +0930

    convert to LED Resource , need to convert UART

--- plat.py
+++ plat.py
@@ -1,6 +1,7 @@
 from nmigen import *
 from nmigen_boards.tinyfpga_bx import *
-from nmigen.build import Resource, Subsignal, Pins
+from nmigen_boards.dev import LEDResources
+from nmigen.build import Resource, Subsignal, Pins, Attrs
 
 "Tiny BX in a BREAD BOARD , with 4 blinky and an FTDI serial "
 
@@ -14,10 +15,7 @@ class BB(TinyFPGABXPlatform):
             Subsignal("tx", Pins("19", conn=("gpio", 0), dir="o")),
             Subsignal("rx", Pins("20", conn=("gpio", 0), dir="i")),
         ),
-        Resource("blinky", 0, Pins("12", conn=("gpio", 0), dir="o")),
-        Resource("blinky", 1, Pins("13", conn=("gpio", 0), dir="o")),
-        Resource("blinky", 2, Pins("14", conn=("gpio", 0), dir="o")),
-        Resource("blinky", 3, Pins("15", conn=("gpio", 0), dir="o")),
+        *LEDResources('blinky',pins="12 13 14 15",attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
         Resource("pwm", 0, Pins("5", conn=("gpio", 0), dir="o")),
     ]
 
