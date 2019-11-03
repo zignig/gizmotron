9710dff50e9001bc8b1ce57cd8763b45505e02e6
Simon Kirkby <obeygiantrobot@gmail.com>
2019-11-03 12:46:53 +0800

    fix up resources

--- plat.py
+++ plat.py
@@ -1,6 +1,6 @@
 from nmigen import *
 from nmigen_boards.tinyfpga_bx import *
-from nmigen_boards.dev import LEDResources
+from nmigen_boards.resources import LEDResources
 from nmigen.build import Resource, Subsignal, Pins, Attrs
 
 "Tiny BX in a BREAD BOARD , with 4 blinky and an FTDI serial "
@@ -15,7 +15,7 @@ class BB(TinyFPGABXPlatform):
             Subsignal("tx", Pins("19", conn=("gpio", 0), dir="o")),
             Subsignal("rx", Pins("20", conn=("gpio", 0), dir="i")),
         ),
-        *LEDResources('blinky',pins="12 13 14 15",attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
+        *LEDResources('blinky',pins="J1 J2 J9 D9",attrs=Attrs(IO_STANDARD="SB_LVCMOS")),
         Resource("pwm", 0, Pins("5", conn=("gpio", 0), dir="o")),
     ]
 
