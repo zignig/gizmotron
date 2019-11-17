048e98912695c2a34b8655ea0bbf7e9fda567853
Simon Kirkby <obeygiantrobot@gmail.com>
2019-11-17 15:14:47 +0800

    register ideas

--- compiler/registers.py
+++ compiler/registers.py
@@ -19,6 +19,54 @@ minimise the amount of spill an copying from one window to the other
 multi register allocations ? 
 pointer deferencing ? 
 
+###
+
+from conversations with tpw_rules and looking at his programming style.
+The use of a frame stack where each subroutine call moves the register window 
+up 8 register.
+
+- Loads the needed registers from the preceeding frame
+- Allocates local variables 
+- Runs the subroutine code
+- Copies return variable down 
+- Shifts the window back down 
+- And then return jumps from the parent frame.
+
+This is quite elegant. A subroutine becomes.
+
+assuming R6 is the Frame pointer (fp)
+
+If you need to pass variables up and down
+
+    LDW(fp,-8) # put the previous window address into R6
+    LD(var1,fp,1) # load the first register of the prevuis frame into var1 
+    LD(var2,fp,2) # second register
+
+    # other code
+    # blah blah blah
+
+    ST(return_val,fp,3) # put the value it register 3 in the frame above
+
+    Then return up
+    ADJW(8) # move the window to the previous frame address
+    JR(R7,0) # the return jump is in the parent frame
+
+If nothing needs to be passed up or down
+
+    ADJW(8)
+    # blah blah blah 
+    ADJW(-8)
+    JR(R7,0)
+
+and you don't lose a register for the frame pointer.
+
+When you want to call a subroutine , it's just
+
+    JAL(R7,'subroutine')
+
+and tada, it runs and puts variables back into the current frame.
+
+Kind of nifty.
 
 """
 
