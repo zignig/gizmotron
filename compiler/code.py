1722d9cf1cbf50dff7cb5ad8dd619fa5bb196cb5
Simon Kirkby <obeygiantrobot@gmail.com>
2019-11-19 09:52:23 +0800

    show output of code test

--- compiler/code.py
+++ compiler/code.py
@@ -1,22 +1,23 @@
-from registers import * 
+from registers import *
 from boneless.arch.opcode import *
+
 
 class Delay(SubR):
     def setup(self):
-        self.params = ['duration']
-        self.locals = ['counter']
+        self.params = ["duration"]
+        self.locals = ["counter"]
 
     def instr(self):
         w = self.w
         ll = LocalLabels()
         return [
-            MOV(w.duration,w.counter),
-            ll('again'),
-            SUBI(w.counter,w.counter,1),
-            CMP(w.duration,w.counter),
+            MOV(w.duration, w.counter),
+            ll("again"),
+            SUBI(w.counter, w.counter, 1),
+            CMP(w.duration, w.counter),
             BZ(ll.exit),
             J(ll.again),
-            ll('exit'),
+            ll("exit"),
         ]
 
 
@@ -24,13 +25,35 @@ class Blinker(Firmware):
     def instr(self):
         delay = Delay()
         w = self.w
-        w.req('delay')
+        w.req("delay")
         delay_count = 8192
-        return [
-            MOVI(w.delay,delay_count),
-            delay(w.delay),
-        ]
+        return [MOVI(w.delay, delay_count), delay(w.delay)]
+
 
 if __name__ == "__main__":
     bl = Blinker()
     print(bl.code())
+
+# 20191119 output ( reformatted )
+c = [
+    MOVI(R6, 4096),
+    Label("main"),
+    [MOVI(R0, 8192), [LD(R0, R6, -8), JAL(R7, "Delay")]],
+    J("main"),
+    Label("ExtraCode"),
+    [
+        [
+            Label("Delay"),
+            LDW(R6, -8),
+            AND(R0, R1, R1),
+            Label("_2666359660_again"),
+            SUBI(R1, R1, 1),
+            CMP(R0, R1),
+            BZ("_2666359660_exit"),
+            J("_2666359660_again"),
+            Label("_2666359660_exit"),
+            ADJW(8),
+            JR(R7, 0),
+        ]
+    ],
+]
