c0b71c9de088abd8000bcdcc25175522ca077bd2
Simon Kirkby <obeygiantrobot@gmail.com>
2019-11-21 18:47:46 +0800

    with bin

--- compiler/code.py
+++ compiler/code.py
@@ -1,6 +1,7 @@
 from registers import *
 from boneless.arch.opcode import *
-
+from boneless.arch.asm import Assembler
+import pprint
 
 class Delay(SubR):
     def setup(self):
@@ -21,39 +22,59 @@ class Delay(SubR):
         ]
 
 
+class LedSet(SubR):
+    def setup(self):
+        self.params = ["value"]
+
+    def instr(self):
+        return [STXA(self.w.value, 0)]
+
+
 class Blinker(Firmware):
     def instr(self):
         delay = Delay()
+        ledset = LedSet()
         w = self.w
         w.req("delay")
+        w.req("lednum")
         delay_count = 8192
-        return [MOVI(w.delay, delay_count), delay(w.delay)]
+        return [
+            MOVI(w.delay, delay_count),
+            delay(w.delay),
+            ledset(w.lednum),
+            ADDI(w.lednum, w.lednum, 1),
+        ]
 
 
 if __name__ == "__main__":
     bl = Blinker()
-    print(bl.code())
+    code = bl.code()
+    pprint.pprint(code)
+    asm = Assembler()
+    asm.parse(code)
+    bin = asm.assemble()
+    print(bin)
 
-# 20191119 output ( reformatted )
-c = [
-    MOVI(R6, 4096),
-    Label("main"),
-    [MOVI(R0, 8192), [LD(R0, R6, -8), JAL(R7, "Delay")]],
-    J("main"),
-    Label("ExtraCode"),
-    [
-        [
-            Label("Delay"),
-            LDW(R6, -8),
-            AND(R0, R1, R1),
-            Label("_2666359660_again"),
-            SUBI(R1, R1, 1),
-            CMP(R0, R1),
-            BZ("_2666359660_exit"),
-            J("_2666359660_again"),
-            Label("_2666359660_exit"),
-            ADJW(8),
-            JR(R7, 0),
-        ]
-    ],
-]
+"""
+[MOVI(R6, 4096),
+ Label('main'),
+ [MOVI(R0, 8192),
+  [LD(R0, R6, -8), JAL(R7, 'Delay')],
+  [LD(R1, R6, -8), JAL(R7, 'LedSet')],
+  ADDI(R1, R1, 1)],
+ J('main'),
+ Label('ExtraCode'),
+ [[Label('Delay'),
+   LDW(R6, -8),
+   AND(R0, R1, R1),
+   Label('again_419233995'),
+   SUBI(R1, R1, 1),
+   CMP(R0, R1),
+   BZ('exit_419233995'),
+   J('again_419233995'),
+   Label('exit_419233995'),
+   ADJW(8),
+   JR(R7, 0)],
+  [Label('LedSet'), LDW(R6, -8), STXA(R0, 0), ADJW(8), JR(R7, 0)]]]
+[49664, 34304, 50176, 32768, 16600, 44804, 16856, 44810, 6433, 49144, 42616, 33, 6449, 25, 47105, 49148, 41032, 42880, 42616, 30720, 41032, 42880]
+"""
