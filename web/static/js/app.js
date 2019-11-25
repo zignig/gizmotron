826e1c2b2ccab257cb9cf8b977f3bf6731d2a2e0
Simon Kirkby <obeygiantrobot@gmail.com>
2019-11-26 07:29:15 +0800

    web interfaces poked

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -40,6 +40,28 @@ Vue.component('nav-blob',
     template: '<div><b-button>Click Me</b-button></div>',
 }); 
 
+Vue.component('compile-steps',
+{ 
+    data() {
+        return {
+            isActive: true 
+        }
+    },
+    props: {
+        issueItem: {
+            type: String,
+        },
+    },
+    template: `  
+    <section>
+        <b-steps size="is-large">
+            <b-step-item label="Account" icon="account"></b-step-item>
+            <b-step-item label="Profile" icon="account"></b-step-item>
+            <b-step-item label="Social" icon="account"></b-step-item>
+        </b-steps>
+    </section>`,
+}); 
+
 vm = new Vue({
     el: '#app',
     data()  {
