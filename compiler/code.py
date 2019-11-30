2099e6209a6fcaeaa6000c159d588f8e8263e7e5
Simon Kirkby <obeygiantrobot@gmail.com>
2019-11-30 15:36:59 +0800

    double bork

--- compiler/code.py
+++ compiler/code.py
@@ -79,38 +79,3 @@ if __name__ == "__main__":
     bin = asm.assemble()
     print(bin)
 
-"""
-[MOVI(R6, 4096),
- Label('main'),
- [MOVI(R0, 8192),
-  [LD(R0, R6, -8), JAL(R7, 'Delay')],
-  [LD(R1, R6, -8), JAL(R7, 'LedSet')],
-  ADDI(R1, R1, 1)],
- J('main'),
- Label('ExtraCode'),
- [[Label('Delay'),
-   LDW(R6, -8),
-   AND(R0, R1, R1),
-   Label('again_419233995'),
-   SUBI(R1, R1, 1),
-   CMP(R0, R1),
-   BZ('exit_419233995'),
-   J('again_419233995'),
-   Label('exit_419233995'),
-   ADJW(8),
-   JR(R7, 0)],
-  [Label('LedSet'), LDW(R6, -8), STXA(R0, 0), ADJW(8), JR(R7, 0)]]]
-[49664, 34304, 50176, 32768, 16600, 44804, 16856, 44810, 6433, 49144, 42616, 33, 6449, 25, 47105, 49148, 41032, 42880, 42616, 30720, 41032, 42880]
-"""
-=======
-            ADDI(w.counter, w.counter, 1),
-            leds(w.counter),
-            MOVI(w.address,5),
-            MOVI(w.count,40),
-            writer(w.address,w.count,w.target)
-        ]
-
-if __name__ == "__main__":
-    bl = Blinker()
-    pprint.pprint(bl.code(), indent=4)
->>>>>>> 7ade2ebf26d42bc59562daf99ad09007bbbae911
