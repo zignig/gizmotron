da83f59f20b516d98c74259d0e0d45f3bbb1f83e
Simon Kirkby <obeygiantrobot@gmail.com>
2020-02-03 18:35:16 +0800

    convert short to a firmware

--- compiler/short.py
+++ compiler/short.py
@@ -6,21 +6,21 @@ import pprint
 
 class Adder(SubR):
     def setup(self):
-        self.params = ["left","right"]
+        self.params = ["left", "right"]
         self.ret = ["sum"]
 
     def instr(self):
         w = self.w
-        return [
-                ADD(w.sum,w.left,w.right),
-        ]
+        return [ADD(w.sum, w.left, w.right)]
 
-adder = Adder()
-w = Window()
-w.req(['a','b','c'])
-code = [
-        Rem(w._name),
-        adder(w.a,w.b,ret=w.c),
-        MetaSub.code()
-        ]
-pprint.pprint(code,width=1,indent=3)
+
+class Short(Firmware):
+    def instr(self):
+        adder = Adder()
+        w = self.w 
+        w.req(["a", "b", "c"])
+        return [adder(w.a, w.b, ret=w.c)]
+
+
+sh = Short()
+sh.show()
