18bb26de6fe453d59366eef817ee64d1f573ade5
Simon Kirkby <obeygiantrobot@gmail.com>
2020-02-15 13:31:14 +0800

    more instr work

--- compiler/instr.py
+++ compiler/instr.py
@@ -10,50 +10,51 @@ import sys
 from lark import Lark, Transformer, v_args
 
 json_grammar = r"""
-    ?start: _NL* content*
+    ?start: _NL* content+
     
-    bs: /\\/                  
-    brac: "{" content* "}" 
-    str: /./+
-    tag: bs NAME brac? -> tag
-    dbs: "\\\\" 
-    comma: ","
-    ampersand: "&"
-    content: tag | brac | NAME | comma | ampersand | dbs | str
+    bs: "\\"
+    ?brac: "{" content* "}" 
+    ?tag: bs NAME brac? -> tag
+    ?dbs: "\\\\" 
+    COMMA: ","
+    MINUS: "-"
+    DOT: "." | "+"
+    AMP: "&"
+    AST:"*"
+    WWA: WORD  AST 
+    ARR: "←"
+    ?text: ARR | WWA | WORD | NUMBER | COMMA | MINUS | DOT |  AMP 
+    ?content:  tag | brac | dbs | text+ 
 
     _NL: /[\r\n]+/
 
     %import common.WS 
+    %import common.WORD
+    %import common.NUMBER
     %ignore WS
     %ignore _NL
     %import common.CNAME -> NAME
 """
 
-
-_json_grammar = r"""
-    ?start: _NL* main 
+class tr(Transformer):
+    def content(self,items):
+        d = ""
+        for i in items:
+            d += str(i)+' '
+        return d
     
-    main: tag 
-    bs: /\\/
-    tag: bs NAME brac?
-    _NL: /[\r\n]+/
-    brac: "{" content+ "}" 
-    content: str
-    str: /./
+#    def text(self,items):
+        print(items)
 
-    %import common.WS 
-    %ignore WS
-    %ignore _NL
-    %import common.CNAME -> NAME
-"""
-json_parser = Lark(json_grammar, parser='lalr')
+json_parser = Lark(json_grammar, parser='lalr',transformer=tr())
 
 parse = json_parser.parse
-file_name = "/opt/FPGA/Boneless-CPU/doc/manual/insns/ADD.tex"
+file_name = "/opt/FPGA/Boneless-CPU/doc/manual/insns/CMPI.tex"
 #file_name = "ADD.tex"
 
 if __name__ == '__main__':
+    print(file_name)
     f = open(file_name)
     t = parse(f.read())
-    print(t.pretty())
+    print(t)
 
