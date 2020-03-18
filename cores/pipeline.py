28a5eaabab764da73588f9a29ceed7d8def21c0c
Simon Kirkby <obeygiantrobot@gmail.com>
2020-03-18 08:46:42 +0800

    pipline builder start

--- cores/pipeline.py
+++ cores/pipeline.py
@@ -2,13 +2,46 @@
 
 from nmigen import *
 import stream
+from nmigen.hdl.rec import Layout
+""" 
+Connecting a pipeline together 
+        m.d.comb += [
+            self.morse.sink.connect(self.input.source),
+            self.output.sink.connect(self.morse.source),
+            self.blink.sink.connect(self.output.source),
+        ]
+"""
 
 class Pipeline:
     def __init__(self,pipeline):
         if type(pipeline) != type([]):
             raise ValueError('must be list')
-        for i in pipeline:
-            print(i)
+        " check the pipeline "
+        for item in pipeline:
+            if hasattr(item,'sink'):
+                print(item.sink)
+
+
+class Widget(Elaboratable):
+    " testing widget for pipeline builds " 
+
+    def __init__(self,layout):
+        self.sink = stream.StreamSink(layout)
+        self.source = stream.StreamSource(layout)
+
+    def elaborate(self,platform):
+        m = Module()
+        m.submodules.sink = self.sink
+        m.submodules.source = self.source 
+
+        return m
+
 
 if __name__ == "__main__":
-    print("pipeline")
+    print("pipeline test")
+    layout = Layout([("data", 8),("stuff",4),("enable",1)])
+    w1 = Widget(layout)
+    w2 = Widget(layout)
+    w3 = Widget(layout)
+    p = Pipeline([w1,w2,w3])
+
